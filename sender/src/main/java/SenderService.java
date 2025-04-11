03c7cc39fce4b3282b475fbb1ffabf1dd0e893ce
sajonaro <alex.kunich@gmail.com>
2025-04-11 20:52:18 +0000

    initial commit

---
+++ sender/src/main/java/SenderService.java
@@ -0,0 +1,9 @@
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+
+public class SenderService {
+  public static void main(String[] args) {
+
+    System.out.println("HELLO WORLD");
+  }
+}
